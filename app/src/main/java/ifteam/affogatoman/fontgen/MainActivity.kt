a5d5811e0f9f3b9cf1828362955928bd5a064129
JJH <colombia2@naver.com>
2020-03-07 18:54:53 +0900

    Added size-choosing and fixed letter-conflicting error

--- app/src/main/java/ifteam/affogatoman/fontgen/MainActivity.kt
+++ app/src/main/java/ifteam/affogatoman/fontgen/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.content.ContextCompat
 import com.obsez.android.lib.filechooser.ChooserDialog
 import kotlinx.android.synthetic.main.setting.*
 import java.io.*
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     var baseBitmap: Bitmap = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888)
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var makeButton: Button
     lateinit var settingButton: Button
     lateinit var pathEditText: EditText
+    lateinit var bitmapSizeSeekBar: SeekBar
 
     var screenImageView: ImageView? = null
     var characterProgressBar: ProgressBar? = null
@@ -46,23 +48,34 @@ class MainActivity : AppCompatActivity() {
     var isAntiAlias: Boolean
     var isOnlyKorean: Boolean
     var fontSize: Int
-    var drawX: Int
-    var drawY: Int
+    var drawPaddingX = 0
+    var drawPaddingY = 0
 
-    internal inner class FontGenOnSeekBarChangeListener(private val mainActivity: MainActivity, private val settingImageView: ImageView) : OnSeekBarChangeListener {
+    var bitmapSize: Int = 256
+
+    internal inner class FontGenOnSeekBarChangeListener(private val mainActivity: MainActivity, private val settingImageView: ImageView?) : OnSeekBarChangeListener {
         override fun onStartTrackingTouch(seekBar: SeekBar) {}
         override fun onProgressChanged(seekBar: SeekBar, progress: Int, z: Boolean) {
             when (seekBar.id) {
                 R.id.sizes ->  mainActivity.settingFontSize = progress
-                R.id.xs -> mainActivity.settingDrawX = progress - 16
-                R.id.ys -> mainActivity.settingDrawY = progress - 16
+                R.id.xs -> mainActivity.settingDrawX = progress - xSeekBar.max/2
+                R.id.ys -> mainActivity.settingDrawY = progress - ySeekBar.max/2
+                R.id.ys -> mainActivity.settingDrawY = progress - ySeekBar.max/2
+                R.id.size_seekbar -> {
+                    val tempSize = bitmapSize
+                    mainActivity.bitmapSize = 2.0.pow(progress + 8).toInt()
+                    mainActivity.fontSize = mainActivity.bitmapSize/16
+                    mainActivity.drawPaddingX = drawPaddingX * bitmapSize / tempSize
+                    mainActivity.drawPaddingY = drawPaddingY * bitmapSize / tempSize
+                    mainActivity.findViewById<TextView>(R.id.size_text).text = "$bitmapSize × $bitmapSize"
+                }
                 else -> { }
             }
-            settingImageView.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY + 13, mainActivity.settingFontSize))
+            settingImageView?.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY, mainActivity.settingFontSize))
         }
 
         override fun onStopTrackingTouch(seekBar: SeekBar) {
-            settingImageView.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY + 13, mainActivity.settingFontSize))
+            settingImageView?.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY, mainActivity.settingFontSize))
         }
 
     }
@@ -77,7 +90,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 R.id.sizep -> {
-                    if (this.mainActivity.sizeSeekBar.progress != 16) {
+                    if (this.mainActivity.sizeSeekBar.progress != this.mainActivity.sizeSeekBar.max) {
                         this.mainActivity.settingFontSize++
                         this.mainActivity.sizeSeekBar.incrementProgressBy(1)
                     }
@@ -89,7 +102,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 R.id.xp -> {
-                    if (this.mainActivity.xSeekBar.progress != 32) {
+                    if (this.mainActivity.xSeekBar.progress != this.mainActivity.xSeekBar.max) {
                         this.mainActivity.settingDrawX++
                         this.mainActivity.xSeekBar.incrementProgressBy(1)
                     }
@@ -101,7 +114,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 R.id.yp -> {
-                    if (this.mainActivity.ySeekBar.progress != 32) {
+                    if (this.mainActivity.ySeekBar.progress != this.mainActivity.ySeekBar.max) {
                         this.mainActivity.settingDrawY++
                         this.mainActivity.ySeekBar.incrementProgressBy(1)
                     }
@@ -110,7 +123,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.only_korean -> this.mainActivity.isOnlyKorean = (view as Switch).isChecked
                 else -> { }
             }
-            sampleImageView.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY + 13, mainActivity.settingFontSize))
+            sampleImageView.setImageBitmap(mainActivity.getSample(mainActivity.settingDrawX, mainActivity.settingDrawY, mainActivity.settingFontSize))
         }
 
     }
@@ -124,37 +137,50 @@ class MainActivity : AppCompatActivity() {
 
         override fun run() {
             try {
+                var startX = drawPaddingX
+                var startY = drawPaddingY
+
                 if (!this.mainActivity.isOnlyKorean) {
                     glyph = 0x00
                     maxCount = 256*256-1
                     i = 0
                     startChar = 0
                 }
-                this.mainActivity.baseBitmap = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888)
+                this.mainActivity.baseBitmap = Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888)
                 this.mainActivity.canvas = Canvas(this.mainActivity.baseBitmap)
                 if (this.mainActivity.typeFace != null)
                     this.mainActivity.paint.typeface = this.mainActivity.typeFace
                 this.mainActivity.paint.color = Color.WHITE
                 this.mainActivity.paint.textSize = fontSize_.toFloat()
                 this.mainActivity.paint.isAntiAlias = this.mainActivity.isAntiAlias
+
+                val piece: Bitmap = Bitmap.createBitmap(this.mainActivity.bitmapSize/16, this.mainActivity.bitmapSize, Bitmap.Config.ARGB_8888)
+                var pieceCanvas = Canvas(piece)
+                val rect = Rect()
+
                 while (i <= maxCount) {
                     var file: File
                     //Thread.sleep(1)
-                    this.mainActivity.canvas.drawText(i.toChar().toString(), this.mainActivity.drawX.toFloat(), this.mainActivity.drawY.toFloat(), this.mainActivity.paint)
-                    this.mainActivity.drawX += 16
-                    val myHandler = this.mainActivity.handler
+                    this.mainActivity.paint.getTextBounds(i.toChar().toString(), 0, 1, rect)
+                    Log.i(TAG, rect.left.toString())
+                    pieceCanvas.drawText(i.toChar().toString(), -rect.left.toFloat(), -rect.top.toFloat(), this.mainActivity.paint)
+                    this.mainActivity.canvas.drawBitmap(piece, startX.toFloat(), startY.toFloat(), null)
+                    piece.eraseColor(Color.TRANSPARENT)
+                    startX += bitmapSize/16
 
-                    myHandler.sendMessage(myHandler.obtainMessage(100, arrayOf(i, maxCount)))
+                    val myHandler = this.mainActivity.handler
+                    myHandler.sendMessage(myHandler.obtainMessage(100, arrayOf(i, maxCount, startChar)))
+
                     if (i == startChar+1) {
                         file = File("${Environment.getExternalStorageDirectory().absolutePath}/FontGen/$current/.nomedia")
                         file.parentFile.mkdirs()
                         file.createNewFile()
                     }
                     if ((i - startChar) % 16 == 15 && i != startChar) {
-                        this.mainActivity.drawX = this.mainActivity.settingDrawX;
-                        this.mainActivity.drawY += 16
+                        startX = drawPaddingX
+                        startY += bitmapSize/16
                     }
-                    if (this.mainActivity.drawY > 254 || i == maxCount) {
+                    if (startY - drawPaddingY >= bitmapSize || i == maxCount) {
                         file = File("${Environment.getExternalStorageDirectory().absolutePath}/FontGen/$current/glyph_"+"%02X".format(glyph)+".png")
                         file.parentFile.mkdirs()
                         val fileOutputStream: OutputStream = FileOutputStream(file)
@@ -165,7 +191,7 @@ class MainActivity : AppCompatActivity() {
                         if (i != maxCount) {
                             this.mainActivity.baseBitmap.eraseColor(Color.TRANSPARENT)
                         }
-                        this.mainActivity.drawY = this.mainActivity.settingDrawY + 13
+                        startY = drawPaddingY
                         glyph++
                     }
                     i++
@@ -180,19 +206,20 @@ class MainActivity : AppCompatActivity() {
     inner class MyHandler : Handler() {
         override fun handleMessage(message: Message) {
             if (message.what == 100) {
-                screenImageView!!.setImageBitmap(baseBitmap)
+                makeButton.text = (message.obj as Array<Int>)[0].toChar().toString()
+                if(((message.obj as Array<Int>)[0]-(message.obj as Array<Int>)[2]) % 64 == 63 || (message.obj as Array<Int>)[0] == (message.obj as Array<Int>)[1])
+                    screenImageView!!.setImageBitmap(baseBitmap)
                 characterProgressDialog!!.setMessage(getString(R.string.current_char)+(message.obj as Array<Int>)[0].toChar().toString())
-                characterProgressBar!!.max = (message.obj as Array<Int>)[1]
-                characterProgressBar!!.incrementProgressBy(1)
-                characterProgressDialog!!.max = (message.obj as Array<Int>)[1]
-                characterProgressDialog!!.incrementProgressBy(1)
+                characterProgressBar!!.max = (message.obj as Array<Int>)[1]-(message.obj as Array<Int>)[2]
+                characterProgressBar!!.progress = (message.obj as Array<Int>)[0]-(message.obj as Array<Int>)[2]
+                characterProgressDialog!!.max = (message.obj as Array<Int>)[1]-(message.obj as Array<Int>)[2]
+                characterProgressDialog!!.progress = (message.obj as Array<Int>)[0]-(message.obj as Array<Int>)[2]
                 if ((message.obj as Array<Int>)[0] == (message.obj as Array<Int>)[1]) {
+                    makeButton.text = getString(R.string.make)
                     makeButton.isEnabled = true
                     settingButton.isEnabled = true
                     characterProgressBar!!.progress = 0
                     characterProgressDialog!!.dismiss()
-                    drawX = 0
-                    drawY = 13
                 }
             }
             super.handleMessage(message)
@@ -206,6 +233,7 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
 
         setContentView(R.layout.main)
+        bitmapSizeSeekBar = findViewById(R.id.size_seekbar)
         characterProgressBar = findViewById(R.id.progress)
         pathEditText = findViewById(R.id.path)
         findButton = findViewById(R.id.find)
@@ -216,7 +244,9 @@ class MainActivity : AppCompatActivity() {
             try {
                 when (view.id) {
                     R.id.find -> {
-                        ChooserDialog(this@MainActivity)
+                        val chooserDialog = ChooserDialog(this@MainActivity)
+                        chooserDialog.withOnBackPressedListener { _ -> chooserDialog.goBack() }
+                                .withOnLastBackPressedListener { dialog -> dialog.cancel() }
                                 .withFilter(false, true, "ttf", "otf")
                                 .withResources(R.string.choose_ttf_file, R.string.choose_ttf_file, R.string.cancel)
                                 .withChosenListener { _, file ->
@@ -248,52 +278,54 @@ class MainActivity : AppCompatActivity() {
         makeButton.setOnClickListener(viewOnClickListener)
         findButton.setOnClickListener(viewOnClickListener)
         settingButton.setOnClickListener(viewOnClickListener)
+        bitmapSizeSeekBar.setOnSeekBarChangeListener(FontGenOnSeekBarChangeListener(this, null))
     }
 
     private fun showSettingDialog() {
         val tempIsAntiAlias: Boolean = isAntiAlias
         val tempIsOnlyKorean: Boolean = isOnlyKorean
-        settingDrawX = drawX
-        settingDrawY = drawY - 13
+        settingDrawX = drawPaddingX
+        settingDrawY = drawPaddingY
         settingFontSize = fontSize
         val inflate = View.inflate(this, R.layout.setting, null)
         val imageView = inflate.findViewById<ImageView>(R.id.screen2)
         sizeSeekBar = inflate.findViewById(R.id.sizes)
         xSeekBar = inflate.findViewById(R.id.xs)
         ySeekBar = inflate.findViewById(R.id.ys)
+
+        sizeSeekBar.max = bitmapSize/16
+        xSeekBar.max = bitmapSize/8
+        ySeekBar.max = bitmapSize/8
+
         antiAliasSwitch = inflate.findViewById(R.id.anti_alias)
         antiAliasSwitch.isChecked = isAntiAlias
         onlyKoreanSwitch = inflate.findViewById(R.id.only_korean)
         onlyKoreanSwitch.isChecked = isOnlyKorean
+
         sizeSeekBar.progress = fontSize
-        xSeekBar.progress = drawX + 16
-        ySeekBar.progress = drawY + 3
-        imageView.setImageBitmap(getSample(drawX, drawY, fontSize))
+        xSeekBar.progress = drawPaddingX + (16*bitmapSize/256)
+        ySeekBar.progress = drawPaddingY + (16*bitmapSize/256)
+
+        imageView.setImageBitmap(getSample(drawPaddingX, drawPaddingY, fontSize))
         val fontGenOnSeekBarChangeListener = FontGenOnSeekBarChangeListener(this, imageView)
         sizeSeekBar.setOnSeekBarChangeListener(fontGenOnSeekBarChangeListener)
         xSeekBar.setOnSeekBarChangeListener(fontGenOnSeekBarChangeListener)
         ySeekBar.setOnSeekBarChangeListener(fontGenOnSeekBarChangeListener)
-        val button = inflate.findViewById<Button>(R.id.sizem)
-        val button2 = inflate.findViewById<Button>(R.id.sizep)
-        val button3 = inflate.findViewById<Button>(R.id.xm)
-        val button4 = inflate.findViewById<Button>(R.id.xp)
-        val button5 = inflate.findViewById<Button>(R.id.ym)
-        val button6 = inflate.findViewById<Button>(R.id.yp)
         val fontGenOnClickListener = FontGenOnClickListener(this, imageView)
-        button.setOnClickListener(fontGenOnClickListener)
-        button2.setOnClickListener(fontGenOnClickListener)
-        button3.setOnClickListener(fontGenOnClickListener)
-        button4.setOnClickListener(fontGenOnClickListener)
-        button5.setOnClickListener(fontGenOnClickListener)
-        button6.setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.sizem).setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.sizep).setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.xm).setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.xp).setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.ym).setOnClickListener(fontGenOnClickListener)
+        inflate.findViewById<Button>(R.id.yp).setOnClickListener(fontGenOnClickListener)
         antiAliasSwitch.setOnClickListener(fontGenOnClickListener)
         onlyKoreanSwitch.setOnClickListener(fontGenOnClickListener)
         val builder = AlertDialog.Builder(this)
                 .setView(inflate)
                 .setTitle(R.string.setting_title)
                 .setPositiveButton(R.string.confirm) { _, _ ->
-                    drawX = settingDrawX
-                    drawY = settingDrawY + 13
+                    drawPaddingX = settingDrawX
+                    drawPaddingY = settingDrawY
                     fontSize = settingFontSize
                 }
                 .setNegativeButton(R.string.cancel) { _, _ ->
@@ -309,8 +341,8 @@ class MainActivity : AppCompatActivity() {
         thread.start()
     }
 
-    fun getSample(_drawX: Int, _drawY: Int, fontSize: Int): Bitmap {
-        val createBitmap = Bitmap.createBitmap(16, 16, Bitmap.Config.ARGB_8888)
+    fun getSample(paddingX: Int, paddingY: Int, fontSize: Int): Bitmap {
+        val createBitmap = Bitmap.createBitmap(bitmapSize/16, bitmapSize/16, Bitmap.Config.ARGB_8888)
         createBitmap.eraseColor(Color.WHITE)
         val canvas = Canvas(createBitmap)
         val paint_ = Paint()
@@ -319,7 +351,9 @@ class MainActivity : AppCompatActivity() {
         paint_.color = Color.BLACK
         paint_.textSize = fontSize.toFloat()
         paint_.isAntiAlias = isAntiAlias
-        canvas.drawText("가", _drawX.toFloat(), _drawY.toFloat(), paint_)
+        val rect = Rect()
+        paint_.getTextBounds("가", 0, 1, rect)
+        canvas.drawText("가", -rect.left.toFloat()+paddingX, -rect.top.toFloat()+paddingY, paint_)
         return Bitmap.createScaledBitmap(createBitmap, 256, 256, false)
     }
 
@@ -348,10 +382,8 @@ class MainActivity : AppCompatActivity() {
         canvas = Canvas(baseBitmap)
         paint = Paint()
         handler = MyHandler()
-        fontSize = 16
+        fontSize = bitmapSize/16
         isAntiAlias = true
         isOnlyKorean = true
-        drawX = 0
-        drawY = 13
     }
 }
